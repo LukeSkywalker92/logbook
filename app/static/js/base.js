497ebbfc174e39412e7cd81631d7d684fe55e7c8
Luke <luke@lukecodewalker.de>
2020-08-24 20:02:07 +0200

    towards terminal

---
+++ app/static/js/base.js
@@ -0,0 +1,22 @@
+var path = window.location.pathname.split('/')
+
+path.pop()
+path.shift()
+
+var navLink = document.getElementById('nav-link');
+var link = '';
+var a = document.createElement('a');
+a.href = '/';
+a.innerHTML = 'home';
+a.classList = 'menu-item';
+navLink.appendChild(a);
+
+for (var p in path) {
+    link += '/'+path[p];
+    var a = document.createElement('a');
+    a.href = link;
+    a.innerHTML = '/'+path[p];
+    a.classList = 'menu-item';
+    navLink.appendChild(a);
+    console.log(link);
+}
